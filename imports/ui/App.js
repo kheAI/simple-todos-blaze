7232ae3019820c744323747aec42f5533469e186
Kai Chew <kafechew@gmail.com>
2026-04-30 15:51:51 +0800

    Add Checkbox

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { TasksCollection } from "../api/TasksCollection"; 
 import '/imports/api/TasksMethods.js'; // this import in this client UI allows for optimistic execution
 import './App.html';
+import './Task.html';
 
 Template.mainContainer.onCreated(function mainContainerOnCreated() {
   Meteor.subscribe('tasks');
