f5cec66223471e4a6629d7e07d9b9ad738406c48
Kai Feng Chew <kafechew@gmail.com>
2026-04-30 19:04:59 +0800

    Remove tasks

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,14 +1,20 @@
-import { Template } from 'meteor/templating';
+import { Template } from "meteor/templating";
 import { TasksCollection } from "../api/TasksCollection";
-import '/imports/api/TasksMethods.js'; // this import in this client UI allows for optimistic execution
-import './Task.html';
-
+import "/imports/api/TasksMethods.js"; // this import in this client UI allows for optimistic execution
+import "./Task.html";
 
 Template.task.events({
-  'click .toggle-checked'() {
+  "click .toggle-checked"() {
     // Set the checked property to the opposite of its current value
     let taskID = this._id;
     let checkedValue = Boolean(this.isChecked);
-    Meteor.callAsync("tasks.toggleChecked", { _id: taskID, isChecked: checkedValue });
+    Meteor.callAsync("tasks.toggleChecked", {
+      _id: taskID,
+      isChecked: checkedValue,
+    });
+  },
+  "click .delete"() {
+    let taskID = this._id;
+    Meteor.callAsync("tasks.delete", { _id: taskID });
   },
 });
