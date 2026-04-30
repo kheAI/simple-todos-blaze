33bde71c5c48d6cbbb570e4670ed60ea1697104e
Kai Feng Chew <kafechew@gmail.com>
2026-04-30 20:07:46 +0800

    Create User Account

--- server/main.js
+++ server/main.js
@@ -1,9 +1,20 @@
 import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
 import { TasksCollection } from "/imports/api/TasksCollection";
 import "../imports/api/TasksPublications";
-import "../imports/api/TasksMethods"; 
+import "../imports/api/TasksMethods";
+
+const SEED_USERNAME = "meteorite";
+const SEED_PASSWORD = "password";
 
 Meteor.startup(async () => {
+  if (!(await Accounts.findUserByUsername(SEED_USERNAME))) {
+    await Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
   if ((await TasksCollection.find().countAsync()) === 0) {
     [
       "First Task",
@@ -14,10 +25,10 @@ Meteor.startup(async () => {
       "Sixth Task",
       "Seventh Task",
     ].forEach((taskName) => {
-      Meteor.callAsync("tasks.insert", { 
-        text: taskName, 
-        createdAt: new Date(), 
-      });      
+      Meteor.callAsync("tasks.insert", {
+        text: taskName,
+        createdAt: new Date(),
+      });
     });
   }
 });
